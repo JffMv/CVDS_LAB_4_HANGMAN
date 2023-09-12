cc9b34956050063b83f57d94a929e1dbbf734a71
Jefferson Mesa <mesajefer2@gmail.com>
2023-09-11 23:31:54 -0500

    add dependency the pom

--- src/main/java/hangman/model/GameScore.java
+++ src/main/java/hangman/model/GameScore.java
@@ -3,5 +3,6 @@ package hangman.model;
 import com.google.inject.Inject;
 
 public interface GameScore {
+    
     public int calculateScore(int correctCount,int incorrectCount) throws ScoreExcetion;
 }
--- src/main/java/hangman/model/OriginalScore.java
+++ src/main/java/hangman/model/OriginalScore.java
@@ -2,6 +2,7 @@ package hangman.model;
 
 public class OriginalScore implements GameScore {
     
+    
     public int calculateScore(int correctCount, int incorrectCount) throws ScoreExcetion{
         if (correctCount < 0 || incorrectCount < 0) throw new ScoreExcetion(ScoreExcetion.NEGATIVE_EXCEPTION_SCORE);
         int count = 100-(incorrectCount*10);
